69927957c9636c6278565cb96ef800fbe33928f7
Сергей <smk-sbobryka@mail.ru>
2020-10-09 20:50:54 +0300

    Создал модель представления главного окна

--- CV19/ViewModels/ViewModelMainWindow.cs
+++ CV19/ViewModels/ViewModelMainWindow.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace CV19.ViewModels
 {
-    class ViewModelMainWindow
+    internal class ViewModelMainWindow : ViewModelBase
     {
+        #region Заголовок окна
+
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title 
+        { 
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
