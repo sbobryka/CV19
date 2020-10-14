b4d72cf05dc37fb91c1fc85cc2d4cfbca6badfa2
Сергей Бобрыка <sbobryka@outlook.com>
2020-10-14 14:51:56 +0300

    Создал модели студента и группы

---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace CV19.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+    }
+}
