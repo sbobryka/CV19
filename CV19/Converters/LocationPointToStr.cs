d1fc31c8005484d12140f376f2c736ab658f69cc
Сергей Бобрыка <sbobryka@outlook.com>
2020-11-18 12:03:36 +0300

    Добавил конвертер для форматирования координат (LocationPoint)

---
+++ CV19/Converters/LocationPointToStr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CV19.Converters
+{
+    internal class LocationPointToStr : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Point point)) return null;
+
+            return $"{point.X:f3} : {point.Y:f3}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
