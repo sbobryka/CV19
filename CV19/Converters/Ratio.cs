94e6032264a97669d5a5708f0012d3a4e37f4749
Сергей Бобрыка <sbobryka@outlook.com>
2020-11-24 17:23:16 +0300

    Тестирование конвертера с коэффициенотом

---
+++ CV19/Converters/Ratio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CV19.Converters
+{
+    internal class Ratio : IValueConverter
+    {
+        [ConstructorArgument("K")]
+        public double K { get; set; } = 1;
+
+        public Ratio() { }
+
+        public Ratio(double k) => K = k;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            return System.Convert.ToDouble(value, culture) * K;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value.ToString())) return null;
+            return System.Convert.ToDouble(value, culture) / K;
+        }
+    }
+}
