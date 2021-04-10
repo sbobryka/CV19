a08c7072fd76e42e7e778d2e358a7437272c2bba
Сергей <smk-sbobryka@mail.ru>
2021-04-10 19:48:32 +0300

    Добавил мультиконвертер и конвертер в кисть

---
+++ CV19/Converters/MultiConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+        public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotSupportedException("Обратное преобразование не поддерживается");
+    }
+}
---
+++ CV19/Converters/ToBrush.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Media;
+
+namespace CV19.Converters
+{
+    internal class ToBrush : MultiConverter
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 3) return null;
+
+            byte[] RGB = new byte[3];
+            RGB[0] = System.Convert.ToByte(values[0]);
+            RGB[1] = System.Convert.ToByte(values[1]);
+            RGB[2] = System.Convert.ToByte(values[2]);
+
+            var color = Color.FromRgb(RGB[0], RGB[1], RGB[2]);
+            var brush = new SolidColorBrush(color);
+            return brush;
+        }
+    }
+}
