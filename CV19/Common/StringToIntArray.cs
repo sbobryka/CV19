9efbfddd154d517f213b76fcb7e65cf22fd56171
Сергей <smk-sbobryka@mail.ru>
2021-04-11 19:51:45 +0300

    Расширение разметки

---
+++ CV19/Common/StringToIntArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+
+namespace CV19.Common
+{
+    [MarkupExtensionReturnType(typeof(int[]))]
+    internal class StringToIntArray : MarkupExtension
+    {
+        [ConstructorArgument("Str")]
+        public string Str { get; set; }
+        [ConstructorArgument("Separator")]
+        public char Separator { get; set; } = ';';
+
+        public StringToIntArray() { }
+        public StringToIntArray(string str) => Str = str;
+        public StringToIntArray(string str, char separator) : this(str) => Separator = separator;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) =>
+            Str.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries).DefaultIfEmpty().Select(int.Parse).ToArray();
+    }
+}
---
+++ TestWpf/ToString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace TestWpf
+{
+    internal class ToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = System.Convert.ToByte(value);
+            return result.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ToStringMulti : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = System.Convert.ToByte(values[0]);
+            return result.ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
